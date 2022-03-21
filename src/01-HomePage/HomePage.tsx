eb4c4a436d34ffd23d5c136ab0d2cd201d47cb16
Andrew Wei <andrewwei913@gmail.com>
2022-03-21 16:15:47 -0700

    Homepage Content Added

--- src/01-HomePage/HomePage.tsx
+++ src/01-HomePage/HomePage.tsx
@@ -1,6 +1,15 @@
 import "./HomePage.css";
 
 function HomePage() {
-  return <div id="HomePage">HomePage is working!</div>;
+  return (
+    <div id="HomePage">
+      <div id="Hi">Hi, my name is Andrew.</div>
+      <div id="About">
+        I enjoy building user facing applications from conceptualization to
+        release.
+      </div>
+      <img src="./public/sunflower.svg" alt="sunflower not loading :(" />
+    </div>
+  );
 }
 export default HomePage;
