60bfdd031807a41fd641608e1f277e4c3fd03021
Andrew Wei <andrewwei913@gmail.com>
2022-03-21 16:29:34 -0700

    Photo added

--- src/01-HomePage/HomePage.tsx
+++ src/01-HomePage/HomePage.tsx
@@ -3,12 +3,16 @@ import "./HomePage.css";
 function HomePage() {
   return (
     <div id="HomePage">
-      <div id="Hi">Hi, my name is Andrew.</div>
-      <div id="About">
-        I enjoy building user facing applications from conceptualization to
-        release.
+      <div id="left">
+        <div id="Hi">Hi, my name is Andrew.</div>
+        <div id="About">
+          I enjoy building user facing applications from conceptualization to
+          release.
+        </div>
       </div>
-      <img src="./public/sunflower.svg" alt="sunflower not loading :(" />
+      <div id="right">
+        <img src="sunflower.svg" alt="sunflower not loading :(" />
+      </div>
     </div>
   );
 }
